2db863c4d252a32b67efb9878f0bbcb65ccd86ce
mayurr07 <49841207+mayurr07@users.noreply.github.com>
2021-11-09 19:41:23 +0530

    Update database.php

--- Dugen/forms/database.php
+++ Dugen/forms/database.php
@@ -5,7 +5,7 @@ define('PASSWORD', 'yVHas5ReFu');
 define('HOST', 'remotemysql.com');
 define('DATABASE', 'pKLgMI4MKp');
 try {
-    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE,";dbuser=".USER,";dbpass=".PASSWORD);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
