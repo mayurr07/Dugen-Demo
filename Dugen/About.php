3cc7f9a4b13c6e8c52f751b63920a1bcc6a3bc6d
mayurr07 <49841207+mayurr07@users.noreply.github.com>
2021-11-09 18:35:16 +0530

    Add files via upload

---
+++ Dugen/About.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Inner Page - Techie Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <!-- <link href="assets/css/style.css" rel="stylesheet"> -->
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css?<?php ?>" />
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center justify-content-between">
+      <h1 class="logo"><a href="./index.php">Dugen</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="./index.php">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <!-- <li><a class="nav-link scrollto" href="#products">products</a></li> -->
+          <!-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li> -->
+          <li class="dropdown"><a href="#"><span>products</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="./medicine.php">Medicine</a></li>
+              <!-- <li class="dropdown"><a href="#"><span>Medicine</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">TABLETS</a></li>
+                  <li><a href="#">CAPSULES</a></li>
+                  <li><a href="#">ORAL LIQUIDS</a></li>
+                  <li><a href="#">INJECTIONS</a></li> -->
+              <!-- <li><a href="#">Deep Drop Down 5</a></li> -->
+              <!-- </ul> -->
+              <!-- </li> -->
+              <!-- <li><a href="#">INTERMEDIATE</a></li> -->
+              <!-- <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li> -->
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="./Gallery.html">Gallery</a></li>
+          <li><a class="nav-link scrollto" href="./index.html">Contact</a></li>
+          <li><a class="getstarted scrollto" href="./index.html">Get Started</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <!-- <section class="breadcrumbs">
+      <div class="container">
+      </div>
+    </section> -->
+    <!-- End Breadcrumbs -->
+
+    <section class="inner-page">
+      <section id="about" class="about">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="150">
+              <img src="assets/img/about2.png" class="img-fluid animated" alt="">
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+              <h3>About us</h3>
+              <p class="Futura">
+                ABOUT US
+                Dugen Pharma is a leading Pharma company in India, dealing with a large variety of Pharmaceutical
+                products
+                and Medical devices. Our company is a pioneer in providing the best products to all at a reasonable
+                price.
+                As per the sincere guidance of our founder Mr. Balasaheb Kakade as in the year 2019, we embarked on a
+                journey to manufacture pharmacceuticals products . With a leader who has 15 years’ experience working in
+                Pharma Marketing background, we have successfully made an impact on Pharmaceutical sector of India in
+                all
+                aspects. We are now expanding our business into ROW market.
+            </div>
+          </div>
+      </section>
+    </section>
+    <section id="about" class="mission">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="150">
+            <img src="assets/img/./29.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3>MISSION</h3>
+            <p class="Futura">
+              To uphold our social responsibilities of delivering highest standard service to all segments of
+              society
+              without compromising on our core values of integrity, good ethics & commitment
+            <h3>Vision </h3>
+            <p class="Futura">
+              To become a global pharmaceutical organization and to be recognized as internationally integrated
+              pharmaceuticals company through innovation, Quality & Competence. To become the leading
+              pharmaceutical
+              player in industry by assuring high quality and cost effective products.
+          </div>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </section>
+   
+</main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <!-- <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Dugen</h3>
+            <p>
+              Lorem ipsum dolor sit.<br>
+              Lorem, ipsum dolor.<br>
+              Lorem, ipsum. <br><br>
+              <strong>Phone:</strong> +09 9999 2222 9<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div> -->
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h3>dugen</h3>
+            <ul>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">products</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li> -->
+              Armed with the vision to develop a global pharmaceutical company, Dugen Pharma constantly endeavors to
+              identify key business players with an edge.
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Contact</h3>
+            <p>
+              Lorem ipsum dolor sit.<br>
+              Lorem, ipsum dolor.<br>
+              Lorem, ipsum. <br><br>
+              <strong>Phone:</strong> +09 9999 2222 9<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h3>works at</h3>
+            <ul>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur aut in reprehenderit vero
+              accusantium aliquam voluptatibus illo impedit corporis. Sed!
+            </ul>
+          </div>
+          <!-- <div class="col-lg-4 col-md-6 footer-newsletter">s
+            <h4>Join Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div> -->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="copyright-wrap d-md-flex py-4">
+        Designed by unknowns <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      </div>
+    </div>
+    <div class="social-links text-center text-md-right pt-3 pt-md-0">
+      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+      <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+    </div>
+    </div>
+
+    </div>
+  </footer>>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
