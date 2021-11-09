f99841e07a9e460885ea832a733564ba33f58d41
mayurr07 <49841207+mayurr07@users.noreply.github.com>
2021-11-09 18:53:50 +0530

    Update medicine.php

--- Dugen/medicine.php
+++ Dugen/medicine.php
@@ -8,7 +8,7 @@ include('./forms/fetch/fetchtablet.php')
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Portfolio Details - Techie Bootstrap Template</title>
+  <title>Medicines</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
